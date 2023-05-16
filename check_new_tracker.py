bf4b7f548a1efe59da5ed8880e541db2dfa307bb
Vasyukov Nikolay <vasyukovn@gmail.com>
2023-05-16 16:27:59 +0300

    Update-2

--- check_new_tracker.py
+++ check_new_tracker.py
@@ -2,8 +2,10 @@ import data
 import sender_stand_request
 
 
+def test_possitive_assert(track_id): # Пока сам не понял что это :)
+    body_track = sender_stand_request.get_new_order(track_id)
+    track_response = "t=" + sender_stand_request.get_order_by_track(body_track)
 
-
-
+    assert track_response.status_code == 200
 
 
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,15 +7,24 @@ import data
 def post_new_order(body):  # Функция создает новый заказ
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
                          json=body)
+
 res = post_new_order(data.body_order)
 print(res.json())
 
-def get_order_by_track(track_order):
+def get_new_order(): # Функция сохраняет номер трека
+    order_body = data.body_order.copy()
+    response = post_new_order(order_body)
+    track = response.json()["track"]
+    return track
+print(get_new_order())
+
+def get_order_by_track(new_track): # Функция на запрос заказа по номеру
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER,
-                        params=track_order)
-resp = get_order_by_track("111222")
+                        params=new_track)
+resp = get_order_by_track("t=" + "670503")
 print(resp.url)
 
+assert resp.status_code == 200
 
 
 
