a1320b0fa0cba2682581faad013aebd0d50d51fc
Vasyukov Nikolay <vasyukovn@gmail.com>
2023-05-18 08:42:51 +0300

    Update-6

--- check_new_tracker.py
+++ check_new_tracker.py
@@ -1,7 +1,10 @@
 import sender_stand_request
 
 
-def test_number_one(new_track=sender_stand_request.get_new_order()):
-    track_resp = sender_stand_request.get_order_by_track(new_track=sender_stand_request.get_new_order())
+def test_number_one():
+    new_track = sender_stand_request.get_new_order()
+    track_resp = sender_stand_request.get_order_by_track(new_track)
+
 
     assert track_resp.status_code == 200
+
--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://0928a984-7a99-4bd4-9bff-0c0fb4e716bc.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://0bd64766-67ff-4c23-94f8-e7723ff5523f.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
 GET_ORDER = "/api/v1/orders/track"
